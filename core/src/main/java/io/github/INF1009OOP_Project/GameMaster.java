419e5433c3c62ec0dc748b915a783e0370d61ed9
zhenhuilee <zhenhuilee17@gmail.com>
2026-02-04 16:07:14 +0800

    Added different scenes

---
+++ core/src/main/java/io/github/INF1009OOP_Project/GameMaster.java
@@ -0,0 +1,34 @@
+package io.github.INF1009OOP_Project;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+
+public class GameMaster extends Game {
+
+	SpriteBatch batch;
+	ShapeRenderer shapeRenderer;
+	BitmapFont font;
+
+	@Override
+	public void create() {
+
+		
+		batch = new SpriteBatch();
+		shapeRenderer = new ShapeRenderer();
+		font = new BitmapFont();
+		setScreen(new StartScreen(this));
+
+	}
+
+	
+	public void dispose() {
+		batch.dispose();
+		shapeRenderer.dispose();
+		font.dispose();
+	}
+
+}

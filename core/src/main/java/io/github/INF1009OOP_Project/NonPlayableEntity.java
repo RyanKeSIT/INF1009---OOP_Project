81a00a2862d2bd78dd015a99c0314ce5309c2d25
PotatoKoh <gabrielkohz.26@gmail.com>
2026-02-05 20:26:55 +0800

    Creation of iMoveable, Playable and NonPlayable Entities

---
+++ core/src/main/java/io/github/INF1009OOP_Project/NonPlayableEntity.java
@@ -0,0 +1,57 @@
+package io.github.INF1009OOP_Project;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class NonPlayableEntity extends Entity implements iMoveable {
+
+	private Texture texture;
+	private float speed;
+	
+	public NonPlayableEntity() {
+		super();
+	}
+	
+	public NonPlayableEntity(float x,float y,float height, float width,Texture texture,float speed) {
+		super(x,y,height,width);
+		this.texture = texture;
+		this.speed = speed;
+	}
+	
+	public Texture getTexture() {
+		return texture;
+	}
+	
+	public void setTexture(Texture texture) {
+		this.texture = texture;
+	}
+	
+	public float getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(float speed) {
+		this.speed = speed;
+	}
+	
+	public void draw(SpriteBatch spritebatch) {
+		
+	}
+	
+	public void getBounds(Bounds bounds) {
+		
+	}
+	
+	public void onCollision(Entity collidable) {
+		
+	}
+	
+	public void move(float x,float y,float speed,float delta) {
+		setSpeed(speed);
+		float tempDropY = super.getY();
+		tempDropY -= getSpeed() * delta;
+		super.setX(x);
+		super.setY(tempDropY);
+	}
+
+}
---
+++ core/src/main/java/io/github/INF1009OOP_Project/iMoveable.java
@@ -0,0 +1,5 @@
+package io.github.INF1009OOP_Project;
+
+public interface iMoveable {
+	void move(float x,float y,float speed,float delta);
+}

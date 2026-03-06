0fede665721ef7949f7e04b4c8e2d7e298f639ec
RyanKnackGAME <emortalswag3@gmail.com>
2026-03-06 14:03:41 +0800

    WIP: Trying out questions and rendeirng questions

--- core/src/main/java/io/github/INF1009OOP_Project/Scenes/GameScene.java
+++ core/src/main/java/io/github/INF1009OOP_Project/Scenes/GameScene.java
@@ -17,120 +17,140 @@ import io.github.INF1009OOP_Project.Engine.IO.IOManager;
 import io.github.INF1009OOP_Project.Engine.Scene.Scene;
 import io.github.INF1009OOP_Project.Engine.Scene.SceneManager;
 
-
 public class GameScene extends Scene {
     private ShapeRenderer shape;
-    private Entity player,bullet;
     private Texture playerTexture;
     private Texture bulletTexture;
     private Texture obstacleTexture;
-    
-	public GameScene(SceneManager sceneManager, IOManager io) {
+    private String[] questions;
+    private Entity player, bullet, bucket;
+    private float consoleTimer;
+
+    public GameScene(SceneManager sceneManager, IOManager io) {
         super(sceneManager, io);
+
         shape = new ShapeRenderer();
         playerTexture = new Texture(Gdx.files.internal("Ship.png"));
         bulletTexture = new Texture(Gdx.files.internal("Bullet.png"));
         obstacleTexture = new Texture(Gdx.files.internal("bucket.png"));
-        		
         io.getSound().soundOn();
+
         initializeGame();
     }
 
     @Override
     public void update() {
-    	
+
         if (io.getKeyboard().isKeyPressed(Keys.ENTER)) {
             // if player die or game ends, push end scene
-        	 sceneManager.push(new EndScene(sceneManager, io));
+            sceneManager.push(new EndScene(sceneManager, io));
         }
 
-        if(io.getKeyboard().isKeyPressed(Keys.ESCAPE)) {
-        	System.out.println("Pause game");
-        	// push pause scene
-        	 sceneManager.push(new PauseScene(sceneManager, io));
+        if (io.getKeyboard().isKeyPressed(Keys.ESCAPE)) {
+            System.out.println("Pause game");
+            // push pause scene
+            sceneManager.push(new PauseScene(sceneManager, io));
         }
-
         if (io.getKeyboard().isKeyJustPressed(Keys.SPACE)) {
             shoot();
             io.getSound().playShootingSound();
         }
-        
-        
+
+        // Update entities based on game tick time
         float delta = Gdx.graphics.getDeltaTime();
         entityManager.updateEntities(delta);
+
+        // Wait a bit until the question renders
+        consoleTimer += delta;
+        System.out.println(consoleTimer);
+        if (consoleTimer > 5) {
+            // Create questions as obstacles
+            float max = 4;
+            for (float i = 0, x = 0; i < max; i++) {
+                // Register text question
+                if (i == 0)
+                    entityManager.addEntity(
+                            new Text(100, 400, 200, 50, "Q: THIS IS A TEST QUESTION...", 25, Color.WHITE, font), false);
+
+                // Create a question block as an entity
+                Entity questionEntity = EntityFactory.createObstacle(x, 300, 50, 50, obstacleTexture);
+                // Get extents
+                Bounds pqe = questionEntity.get(PhysicsBody.class).getBounds();
+                // Evenly spread x through entire game bound
+                x += 750 / max + pqe.getWidth();
+
+                // Register entity
+                entityManager.addEntity(questionEntity, true);
+            }
+
+            consoleTimer = 0;
+        }
+
+        //
     }
 
     @Override
     public void render() {
-    	ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-    	float delta = Gdx.graphics.getDeltaTime();
-    	
-    	ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
         batch.begin();
         batch.end();
-    	
-    	entityManager.draw(batch);
-    	
-    	//visualize bounds hitbox
-    	shape.begin(ShapeRenderer.ShapeType.Line);
-    	
-    	for (Entity entity : entityManager.getEntities()) {  
-    	    PhysicsBody pb = entity.get(PhysicsBody.class);
-    	    if (pb != null) {
-    	        Bounds b = pb.getBounds();
-    	        shape.rect(b.getX(), b.getY(), b.getWidth(), b.getHeight());
-    	    }
-    	}
 
+        entityManager.draw(batch);
+
+        // visualize bounds hitbox
+        shape.begin(ShapeRenderer.ShapeType.Line);
+        for (Entity entity : entityManager.getEntities()) {
+            PhysicsBody pb = entity.get(PhysicsBody.class);
+            if (pb != null) {
+                Bounds b = pb.getBounds();
+                shape.rect(b.getX(), b.getY(), b.getWidth(), b.getHeight());
+            }
+        }
         shape.end();
     }
 
+    // methods
 
-    
-    //methods
-    
     private void initializeGame() {
         // clear old entities first
         entityManager.clearAll();
-        
-        //UI
-        entityManager.addEntity(new Text(300, 300, 200, 50, "Escape to pause!", 50,Color.WHITE, font), false);
-        entityManager.addEntity(new Text(300, 400, 200, 50, "Enter to end game!", 50,Color.WHITE, font), false);
-        
-        //create player and bullet
-        player = EntityFactory.createPlayer(100,100,100,100, playerTexture,bulletTexture,entityManager, 100, io);
-        bullet = EntityFactory.createObstacle(100, 400, 70, 70, obstacleTexture);
-        
-        entityManager.addEntity(player,true);
-        entityManager.addEntity(bullet,true);
-        
+
+        // UI
+        entityManager.addEntity(new Text(400, 0, 200, 50, "Escape to pause!", 20, Color.WHITE, font), false);
+        entityManager.addEntity(new Text(400, 25, 200, 50, "Enter to end game!", 20, Color.WHITE, font), false);
+
+        // Add questions
+
+        // Create player
+        player = EntityFactory.createPlayer(100, 0, 100, 100, playerTexture, bulletTexture, entityManager, 100, io);
+        // Register player entity
+        entityManager.addEntity(player, true);
     }
-    
-    //acts as getter method for initializeGame so startscene can access
+
+    // acts as getter method for initializeGame so startscene can access
     public void resetGame() {
         initializeGame();
     }
-    
+
     private void shoot() {
         Transform pt = player.get(Transform.class);
-        if (pt == null) return;
-        
+        if (pt == null)
+            return;
+
         float bw = 70;
         float bh = 70;
-        
+
         float bx = pt.getX() + pt.getWidth() / 2f - bw / 2f;
-        float by = 75 + pt.getY() + pt.getHeight()/2f - 10; //add 75 for it to spawn slightly above player 
-        Entity newBullet = EntityFactory.createBullet(bx, by, 70, 70, 300, bulletTexture,entityManager);
+        float by = 75 + pt.getY() + pt.getHeight() / 2f - 10; // add 75 for it to spawn slightly above player
+        Entity newBullet = EntityFactory.createBullet(bx, by, 70, 70, 300, bulletTexture, entityManager);
 
         entityManager.addEntity(newBullet, true);
-
     }
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-    
-    
+    @Override
+    public void dispose() {
+        // TODO Auto-generated method stub
+
+    }
 }
